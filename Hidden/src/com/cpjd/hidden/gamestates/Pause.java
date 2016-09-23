1dedede3ad3554e6a38c01f66d1fc7d2a14b649a
WillDavies <wdavies973@gmail.com>
2016-09-22 22:53:54 -0500

    Working pause menu (press escape)

--- Hidden/src/com/cpjd/hidden/gamestates/Pause.java
+++ Hidden/src/com/cpjd/hidden/gamestates/Pause.java
@@ -2,43 +2,64 @@ package com.cpjd.hidden.gamestates;
 
 import java.awt.AlphaComposite;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 
 import com.cpjd.hidden.gamestate.GameState;
 import com.cpjd.hidden.gamestate.GameStateManager;
 import com.cpjd.hidden.main.GamePanel;
+import com.cpjd.hidden.toolbox.Layout;
 import com.cpjd.hidden.ui.FragmentButton;
 
 public class Pause extends GameState {
 
 	private boolean open;
-	
+
 	// Technical
 	private AlphaComposite composite;
 	private AlphaComposite defaultComposite;
 	private int xmov;
-	
+
 	// UI
 	FragmentButton resume, options, exit;
-	
+
 	public Pause(GameStateManager gsm) {
 		super(gsm);
-		
-		composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.80f);
+
+		composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.70f);
 		defaultComposite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f);
-		
+
 		resume = new FragmentButton("Resume");
 		options = new FragmentButton("Options");
 		exit = new FragmentButton("Exit");
+		resume.setFlip(true);
+		options.setFlip(true);
+		exit.setFlip(true);
+
+		xmov = 100;
 	}
 
-	
 	@Override
 	public void update() {
-		xmov += 8;
-		if(xmov > 500) xmov = 500;
+		if(open) {
+			xmov += 13;
+			if(xmov > 400) xmov = 400;
+		}
+
+		if(resume.isClicked()) {
+			xmov = 100;
+			open = false;
+			resume.setClicked(false);
+		}
 		
+		if(exit.isClicked()) {
+			xmov = 100;
+			gsm.setState(GameStateManager.INTRO);
+			open = false;
+			exit.setClicked(false);
+		}
+
 	}
 
 	@Override
@@ -47,57 +68,70 @@ public class Pause extends GameState {
 		g.setComposite(composite);
 		g.setColor(Color.WHITE);
 		g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);
-		g.setComposite(defaultComposite);
+
+		Font font = g.getFont();
+		Font defaultFont = font;
+		font = font.deriveFont(40f);
+		g.setFont(font);
+		g.setColor(Color.BLACK);
+		g.drawString("P A U S E D", Layout.centerString("P A U S E D", g) - GamePanel.WIDTH / 2, Layout.aligny(15));
+		
+		g.setFont(defaultFont);
 		
 		g.setColor(Color.BLACK);
-		g.fillRect(0, (GamePanel.HEIGHT / 2) - 250, xmov, 500);
+		g.fillRect(Layout.centerw(xmov), Layout.centerh(300), xmov, 300);
+
+		resume.draw(g, 10 + Layout.centerw(xmov), Layout.centerh(300) + 50);
+		options.draw(g, 10 + Layout.centerw(xmov), Layout.centerh(300) + 150);
+		exit.draw(g, 10 + Layout.centerw(xmov), Layout.centerh(300) + 250);
 		
-		resume.draw(g, 10, (GamePanel.HEIGHT / 2) - 200);
-		
-		
+		g.setComposite(defaultComposite);
+
 	}
 
 	public boolean isOpen() {
 		return open;
 	}
-	
-	
+
 	@Override
 	public void keyPressed(int k) {
-		if(k == KeyEvent.VK_ESCAPE) open = !open;
-		
-		if(!open) xmov = 0;
-		
+		if(k == KeyEvent.VK_ESCAPE) {
+			if(gsm.getState() > 0) open = !open;
+		}
+
+		if(!open) xmov = 100;
+
 	}
 
 	@Override
 	public void keyReleased(int k) {
-		
-		
+
 	}
 
 	@Override
 	public void mousePressed(int x, int y) {
-		
-		
+		resume.mousePressed(x, y);
+		options.mousePressed(x, y);
+		exit.mousePressed(x, y);
+
 	}
 
 	@Override
 	public void mouseReleased(int x, int y) {
-		
-		
+
 	}
 
 	@Override
 	public void mouseMoved(int x, int y) {
 		resume.mouseMoved(x, y);
-		
+		options.mouseMoved(x, y);
+		exit.mouseMoved(x, y);
+
 	}
 
 	@Override
 	public void mouseWheelMoved(int k) {
-		
-		
+
 	}
-	
+
 }
