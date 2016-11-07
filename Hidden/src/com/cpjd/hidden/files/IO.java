86fb41b875f787fb6ca6a80847959e827e6172ad
WillDavies <wdavies973@gmail.com>
2016-11-07 13:55:53 -0600

    Added Linux and Mac support for the file system

--- Hidden/src/com/cpjd/hidden/files/IO.java
+++ Hidden/src/com/cpjd/hidden/files/IO.java
@@ -20,9 +20,13 @@ public class IO {
 		
 		if(osName.contains("win")) {
 			gameDir = new File((System.getenv("APPDATA") + File.separator + "Hidden" + File.separator));
-			if(!gameDir.exists()) gameDir.mkdir();
+		} else if(osName.contains("mac")) {
+			gameDir = new File(System.getProperty("user.home") + "/Library/Application Support"+File.separator);
+		} else if(osName.contains("nux")) {
+			gameDir = new File(System.getProperty("user.home"));
 		}
 		
+		if(!gameDir.exists()) gameDir.mkdir();
 	}
 	
 	public static boolean serializeGameSave(GameSave gameSave) {
